34ba18eac0a31572f15248f5fa75e7f5febb7d08
Shubham Kumar <76239800+shubham1910200@users.noreply.github.com>
2023-08-21 11:17:21 +0000

    initial-commit

---
+++ 01_basics/03-conversion_and_Operation.js
@@ -0,0 +1,19 @@
+let score = 33;
+console.log(typeof score);
+
+// "33" =>33
+/* "3abc" => NaN
+true =>1; false => 0
+*/
+
+let isLogged = "shbuahm";
+let booleanIsLoggedIn = Boolean(isLogged)
+console.log(booleanIsLoggedIn);
+/* 1 => true
+"" => false
+0 => false
+"shubham" => true
+*/
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
