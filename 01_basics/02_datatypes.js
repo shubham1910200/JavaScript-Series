a81936bffd63d828d63ceaf4533bdc7c7ed59793
Shubham Kumar <76239800+shubham1910200@users.noreply.github.com>
2023-08-21 11:04:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"  // treat all js code as newer version
+
+// document.alert("hello") // we are using node.js not brower
+
+let age = 18
+let isLoggedIn = false;
+let state;
+/*
+datatypes has 7 types=>
+1. number
+2. bigint
+3. string
+4 boolean =>true/false
+5 null => standlone value
+6 undefined
+7. symbol  => unique
+------------------------------------------
+1. object
+2. 
+*/
+console.log(typeof(undefined)) //undefined
+console.log(typeof(null)) // object
+
